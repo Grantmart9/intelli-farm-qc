9a4c429adccc3017473a1a2e1333f05078bdebe4
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2021-07-23 12:08:04 +0200

    Wire up irrigation schedule

--- src/pages/tables/IrrigationSchedule.js
+++ src/pages/tables/IrrigationSchedule.js
@@ -37,6 +37,28 @@ export const IrrigationSchedule = () => {
   if (loading) return <Preloader/>;
   if (error) return <p>Error!</p>;
 
+  const SectionTable = ({section}) =>
+    <Table>
+      <thead className="thead-light">
+        <tr>
+          <th className="border-0">Name</th>
+          <th className="border-0">EC Setpoint</th>
+          <th className="border-0">Runtime</th>
+          <th className="border-0">Start time</th>
+          <th className="border-0">End time</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td className="border-0">{section.name}</td>
+          <td className="border-0">{section.ec_setpoint}</td>
+          <td className="border-0">{section.run_time}</td>
+          <td className="border-0">{section.start_time}</td>
+          <td className="border-0">{section.end_time}</td>
+        </tr>
+      </tbody>
+    </Table>;
+
   const FertilizerRow = ({fertilizer}) => 
     <tr>
       <td className="border-0">{fertilizer.name}</td>
@@ -45,6 +67,11 @@ export const IrrigationSchedule = () => {
     </tr>;
 
   const FertilizerTable = ({fertilizers}) =>
+    <>
+      <h3
+        class="flex align-items-center align-content-center justify-content-center">
+        Fertilizer
+      </h3>
       <Table>
         <thead className="thead-light">
           <tr>
@@ -54,12 +81,14 @@ export const IrrigationSchedule = () => {
           </tr>
         </thead>
         <tbody>
-          { fertilizers.map((fertilizer, key) => <FertilizerRow key={key} fertilizer={fertilizer}/>) }
+          {fertilizers.map((fertilizer, key) => <FertilizerRow key={key} fertilizer={fertilizer} />)}
         </tbody>
-      </Table>;
+      </Table>
+    </>;
 
-  const SectionRow = ({section}) => 
+  const SectionRow = ({ section }) =>
     <div
+      class="w-full"
       style={{
         border: "1.5px solid #242540",
         borderRadius: "0.1cm",
@@ -69,27 +98,28 @@ export const IrrigationSchedule = () => {
       }}
     >
       <h3
-        class="flex align-items-center align-content-center justify-content-center">
+        class="flex align-items-center align-content-center justify-content-center"
         style={{
           background: "#bbbcbf",
           color: "#43464d",
           fontWeight: "bold",
           border: "1px solid #bbbcbf",
-          borderRadius: "0.09cm",
-        }}
-      >
+          borderRadius: "0.09cm"
+        }}>
         {section.name}
       </h3>
+      <SectionTable section={section} />
+      <FertilizerTable fertilizers={section.fertilizer} />
     </div>;
     
   return (
-    <div style={{ marginLeft: "10rem", marginTop: "2rem" }}>
+    <div>
       <h2
         class="flex align-items-center align-content-center justify-content-center">
         Irrigation Schedule
       </h2>
       <div
-        class="flex align-items-center align-content-center justify-content-center"
+        class="flex flex-col align-items-center align-content-center justify-content-center"
         style={{
           background: "white",
           border: "1px solid black",
