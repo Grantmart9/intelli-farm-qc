a26995b1d042aa88d2d52c8b2b2ac3375a06c07a
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2021-07-26 13:32:47 +0200

    Setup irrigation editable tables

--- src/pages/tables/IrrigationSchedule.js
+++ src/pages/tables/IrrigationSchedule.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import useAxios from "axios-hooks";
 import { useParams } from "react-router-dom";
 import Preloader from "../../components/Preloader";
@@ -22,64 +22,79 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const IrrigationSchedule = () => {
-  const classes = useStyles();
+const sectionColumns = [
+  {
+    field: "name",
+    headerName: "Name",
+    type: "string"
+  },
+  {
+    field: "ec_setpoint",
+    headerName: "EC Setpoint",
+    type: "number"
+  },
+  {
+    field: "run_time",
+    headerName: "Run time",
+    type: "number"
+  },
+  {
+    field: "start_time",
+    headerName: "Start time",
+    type: "dateTime"
+  },
+  {
+    field: "end_time",
+    headerName: "End time",
+    type: "dateTime"
+  }
+].map(column => ({ ...column, flex: 1, editable: true }));
 
-  const { farmId } = useParams();
-  const [{ data, loading, error }] = useAxios(`${API_URL}/-${farmId}/schedule`);
-  if (loading) return <h1>Loading...</h1>;
-
-  if (loading) return <Preloader />;
-  if (error)
-    return (
-      <p>
-        <FontAwesomeIcon icon={faExclamation} />
-      </p>
-    );
-
-  const sectionColumns = [
-    {
-      field: "name",
-      headerName: "Name",
-      type: "string"
-    },
-    {
-      field: "ec_setpoint",
-      headerName: "EC Setpoint",
-      type: "number"
-    },
-    {
-      field: "run_time",
-      headerName: "Run time",
-      type: "number"
-    },
-    {
-      field: "start_time",
-      headerName: "Start time",
-      type: "dateTime"
-    },
-    {
-      field: "end_time",
-      headerName: "End time",
-      type: "dateTime"
+const SectionTable = ({ id, section, onChange = null }) => {
+  const handleEditCellChangeCommited = useCallback(({ field, props: { value } }) => {
+    const editedSection = { ...section, [field]: value }
+    if (onChange) {
+      onChange(editedSection, id);
     }
-  ].map(column => ({...column, editable: true}));
+  }, [section, onChange])
 
-  const SectionTable = ({ section }) => (
-    <DataGrid rows={[section]} columns={sectionColumns} onEditCellChangeCommited={(...args) => console.log("DataGrid callback", ...a)}/>
-  );
+  return (
+    <div className="flex">
+      <DataGrid hideFooter={true} autoHeight rows={[{ id: 0, ...section }]} columns={sectionColumns}
+        onEditCellChangeCommitted={handleEditCellChangeCommited} />
+    </div>
+  )
+}
 
-  const FertilizerRow = ({ fertilizer }) => (
-    <tr>
-      <td className="border-0">
-        <h4>{fertilizer.name}</h4>
-      </td>
-      <td className="border-0 fw-bold">{fertilizer.ec_setpoint}</td>
-      <td className="border-0 fw-bold">{fertilizer.flow_rate}</td>
-    </tr>
-  );
+const fertilizerColumns = [
+  {
+    field: "name",
+    headerName: "Name",
+    type: "string"
+  },
+  {
+    field: "ec_setpoint",
+    headerName: "EC Setpoint",
+    type: "number"
+  },
+  {
+    field: "flow_rate",
+    headerName: "Flow rate",
+    type: "number"
+  },
+].map(column => ({ ...column, flex: 1, editable: true }));
 
-  const FertilizerTable = ({ fertilizers }) => (
+const FertilizerTable = ({ id: sectionId, section, onChange = null }) => {
+  const { fertilizer: fertilizers } = section;
+  const handleEditCellChangeCommited = useCallback(({ id: fertilizerId, field, props: { value } }) => {
+    const editedFertilizer = { ...fertilizers[fertilizerId], [field]: value };
+    const editedFertilizers = [...fertilizers.slice(0, fertilizerId), editedFertilizer, ...fertilizers.slice(fertilizerId + 1)];
+    const editedSection = { ...section, fertilizer: editedFertilizers };
+    if (onChange) {
+      onChange(editedSection, sectionId);
+    }
+  }, [section, onChange]);
+  return (
     <>
       <h3
         style={{
@@ -98,64 +113,97 @@ export const IrrigationSchedule = () => {
       >
         Fertilizer
       </h3>
-      <Table>
-        <thead className="thead-light">
-          <tr>
-            <th className="border-0">Name</th>
-            <th className="border-0">EC Setpoint</th>
-            <th className="border-0">Flow Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {fertilizers.map((fertilizer, key) => (
-            <FertilizerRow key={key} fertilizer={fertilizer} />
-          ))}
-        </tbody>
-      </Table>
+      <DataGrid hideFooter={true} autoHeight
+        rows={fertilizers.map((fertilizer, id) => ({ id: id, ...fertilizer }))}
+        columns={fertilizerColumns}
+        onEditCellChangeCommitted={handleEditCellChangeCommited} />
     </>
   );
+};
 
-  const SectionRow = ({ section }) => (
-    <div
-      class="w-full"
+const SectionRow = ({ id, section, onChange = null }) => (
+  <div
+    className="w-full"
+    style={{
+      border: "1.5px solid #242540",
+      borderRadius: "0.1cm",
+      padding: "1rem",
+      boxShadow: "3px 3px #5b5c75",
+      marginTop: "1rem",
+    }}
+  >
+    <h3
+      class="flex align-items-center align-content-center justify-content-center"
       style={{
-        border: "1.5px solid #242540",
-        borderRadius: "0.1cm",
-        padding: "1rem",
-        boxShadow: "3px 3px #5b5c75",
-        marginTop: "1rem",
+        background: "#bbbcbf",
+        color: "#43464d",
+        fontWeight: "bold",
+        border: "1px solid #bbbcbf",
+        borderRadius: "0.09cm",
       }}
     >
       <h3
-        class="flex align-items-center align-content-center justify-content-center"
         style={{
-          background: "#bbbcbf",
+          background: "#b6b9bf",
           color: "#43464d",
+          height: "3rem",
+          padding: "0.5rem",
           fontWeight: "bold",
-          border: "1px solid #bbbcbf",
-          borderRadius: "0.09cm",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
         }}
       >
-        <h3
-          style={{
-            background: "#b6b9bf",
-            color: "#43464d",
-            height: "3rem",
-            padding: "0.5rem",
-            fontWeight: "bold",
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {section.name}
-        </h3>
+        {section.name}
       </h3>
-      <SectionTable section={section} />
-      <FertilizerTable fertilizers={section.fertilizer} />
-    </div>
+    </h3>
+    <SectionTable id={id} section={section} onChange={onChange} />
+    <FertilizerTable id={id} section={section} onChange={onChange} />
+  </div>
+);
+
+export const IrrigationSchedule = () => {
+  const classes = useStyles();
+
+  const { farmId } = useParams();
+  const [{ data, loading, error }] = useAxios(`${API_URL}/-${farmId}/schedule`);
+  const [schedule, setSchedule] = useState();
+  useEffect(() => {
+    if(data) {
+      setSchedule(data);
+    }
+  }, [data]);
+
+  const [{ loading: posting }, postSchedule] = useAxios(
+    {
+      url: `${API_URL}/-${farmId}/schedule`,
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json',
+      },
+    },
+    {
+      manual: true,
+    }
   );
+
+  if (loading || !schedule) return <Preloader />;
+  if (error)
+    return (
+      <p>
+        <FontAwesomeIcon icon={faExclamation} />
+      </p>
+    );
+
+  const handleChange = async (editedSection, id) => {
+    const editedSchedule = schedule.map(section => 
+      section.sql_index == editedSection.sql_index 
+      ? editedSection
+      : section);
+    await postSchedule({ data: editedSchedule });
+    setSchedule(editedSchedule);
+  }
   return (
     <div>
       <h2
@@ -189,10 +237,18 @@ export const IrrigationSchedule = () => {
             <FontAwesomeIcon icon={faSave} /> Save
           </Button>
         </OverlayTrigger>
-        {data.map((section, key) => (
-          <SectionRow key={key} section={section} />
-        ))}
+        
+        {
+          schedule.map((section, i) => {
+            return (
+              <SectionRow key={i} id={section.sql_index} section={section} onChange={handleChange} />
+            );
+          })
+        }
+        
       </div>
     </div>
   );
 };
+
+
