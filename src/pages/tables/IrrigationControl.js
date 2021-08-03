62b1315fd0df762c24b4da828d38b9cb150d87b5
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2021-08-03 12:24:51 +0200

    Fix renderData/renderError

--- src/pages/tables/IrrigationControl.js
+++ src/pages/tables/IrrigationControl.js
@@ -67,25 +67,25 @@ const IrrigationControl = () => {
           >
             <AxiosSpinner
               callHook={use => use(`${API_URL}/${farmId}/irrigation_1`)}
-              renderData={data => data.map(
+              renderData={({data}) => data.map(
                 irrigation_valve => <EquipmentStatus data={irrigation_valve} />)} />
           </div>
         </div>
         <div className="col-span-3 bg-gray-400  rounded shadow-md m-4">
           <div className="w-full h-full">
             <AxiosSpinner
-              callHook={use => use(`${API_URL}/${farmId}/irrigation_2`)}
-              renderData={data => data.map(
+              callHook={use => use(`${API_URL}/${farmId}/irrigation_3`)}
+              renderData={({data}) => data.map(
                 irrigation_valve => <EquipmentStatus data={irrigation_valve} />)} />
           </div>
           <div className="col-span-3 bg-gray-400  rounded shadow-md m-4">
             <div style={{ backgroundColor: "#dfe2e8" }}>
               <AxiosSpinner
-                callHook={use => use(`${API_URL}/${farmId}/irrigation_3`)}
-                renderData={data =>
+                callHook={use => use(`${API_URL}/${farmId}/irrigation_2`)}
+                renderData={({data}) =>
                   <BrushChart
                     data={data.map(
-                      ({ datetime, y, ...rest }) => ({ ...rest, x: new Date(datetime), y }))} />}
+                      ({ datetime, y, ...rest }) => ({ ...rest, x: new Date(datetime), y: Number(y)}))} />}
               />
             </div>
           </div>
