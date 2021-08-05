98c5ea8b17042e95b7be27fbc50ea0c10c006603
Grant <Grant@LODICONCPT.LOCAL>
2021-08-05 12:08:41 +0200

    Error gif

--- src/pages/tables/Fertilizer.js
+++ src/pages/tables/Fertilizer.js
@@ -5,6 +5,7 @@ import Preloader from "../../components/Preloader";
 import { API_URL } from "../../api";
 import { AppName } from "./AppName";
 import ErrorPage from "./ErrorPage.jpg";
+import ErrorGif from "./ErrorGif.gif";
 
 const Fertilizer = () => {
   const { farmId } = useParams();
@@ -20,7 +21,7 @@ const Fertilizer = () => {
     <div style={{ backgroundColor: "#cad3de" }}>
       <AppName />
       <div className="sm-ml-0 md:ml-8 xl:ml-8 2xl:ml-8 sm:mt-0 md:mt-16 xl:mt-16 2xl:mt-16 sm:p-1 md:p-1 p-1">
-        <img src={ErrorPage} alt={ErrorPage} width="100%" />
+        <img src={ErrorGif} alt={ErrorGif} width="100%" />
       </div>
     </div>
   );
--- src/pages/tables/Notifications.js
+++ src/pages/tables/Notifications.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {AppName} from "./AppName";
 import ErrorPage from "./ErrorPage.jpg";
+import ErrorGif from "./ErrorGif.gif";
 
 export const Notifications = () => {
   return (
     <div style={{ backgroundColor: "#cad3de" }}>
       <AppName />
       <div className="sm-ml-0 md:ml-8 xl:ml-8 2xl:ml-8 sm:mt-0 md:mt-16 xl:mt-16 2xl:mt-16 sm:p-1 md:p-1 p-1">
-        <img src={ErrorPage} alt={ErrorPage} width="100%" />
+        <img src={ErrorGif} alt={ErrorGif} width="100%" />
       </div>
     </div>
   );
