9897470297fc8f91ccca9948f757edc4fa193f5b
Grant Marthinus <grant.marthinus@lodicon.co.za>
2021-08-07 21:31:37 +0200

    Fertilizer optimized

--- src/pages/tables/Fertilizer.js
+++ src/pages/tables/Fertilizer.js
@@ -11,28 +11,16 @@ import ChartViewer from "./LineChart";
 
 const FertilizerValves = ({ valves }) => {
   return (
-    <div>
-      <div className="shadow-md border-1 rounded">
-        <div
-          style={{
-            border: "1px 1px solid black",
-            display: "flex",
-            padding: "1rem",
-            fontFamiliy: "Times New Roman",
-            gap: "1rem",
-          }}
-        >
-          <div>
-            <h4 className="text-gray-700 text-lg font-bold">{valves.name}</h4>
-            <h4 className="text-red-800 font-bold">Alarm Status: {valves.alarm}</h4>
-            <h4 className="text-green-800 text-xl font-bold">
-              Flow: {valves.real_time_flow}
-            </h4>
-            <h4 className="text-gray-800 text-sm font-bold">
-              Total Flow: {valves.total_flow}
-            </h4>
-          </div>
-        </div>
+    <div className="shadow-md rounded p-4">
+      <div className="text-gray-800 text-2xl font-bold">{valves.name}</div>
+      <div className="text-red-800 font-bold text-xl">
+        Alarm Status: {valves.alarm}
+      </div>
+      <div className="text-green-800 text-xl font-bold">
+        Flow: {valves.real_time_flow}
+      </div>
+      <div className="text-gray-800 text-sm font-bold text-md">
+        Total Flow: {valves.total_flow}
       </div>
     </div>
   );
@@ -40,33 +28,20 @@ const FertilizerValves = ({ valves }) => {
 
 const ECValves = ({ ec }) => {
   return (
-    <div>
-      <div className="shadow-md border-1 rounded">
-        <div
-          style={{
-            border: "1px 1px solid black",
-            display: "flex",
-            padding: "1rem",
-            fontFamiliy: "Times New Roman",
-            gap: "1rem",
-          }}
-        >
-          <div>
-            <h4 className="text-gray-800  text-lg font-bold">{ec.name}</h4>
-            <h4 className="text-red-800 font-bold">Alarm status: {ec.alarm}</h4>
-            <h4 className="text-gray-800 text-lg font-bold">
-              Setpoint: {ec.setpoint}
-            </h4>
-            <h4 className="text-green-800 text-lg font-bold">
-              Value: {ec.value}
-            </h4>
-            <h4 className="text-gray-800 text-sm font-bold">Average: {ec.average}</h4>
-          </div>
-        </div>
+    <div className="shadow-md border-1 rounded p-4">
+      <div className="text-gray-800 text-2xl font-bold">{ec.name}</div>
+      <div className="text-red-800 font-bold text-xl">Alarm status: {ec.alarm}</div>
+      <div className="text-green-800 text-xl font-bold">
+        Setpoint: {ec.setpoint}
+      </div>
+      <div className="text-green-800 text-xl font-bold">Value: {ec.value}</div>
+      <div className="text-gray-800 text-sm font-bold">
+        Average: {ec.average}
       </div>
     </div>
   );
 };
+
 export const FertilizerPieChart = ({ data }) => {
   const series = data.map(({ ratio }) => ratio);
   const labels = data.map(({ name }) => name);
@@ -80,7 +55,7 @@ export const FertilizerPieChart = ({ data }) => {
         legend: {
           formatter: (label, { seriesIndex }) =>
             `${label} - ${data[seriesIndex].value} ${data[seriesIndex].unit}`,
-          position: "bottom",
+          position: "bottom"
         },
         title: {
           text: "Fertilizer",
@@ -88,9 +63,9 @@ export const FertilizerPieChart = ({ data }) => {
           offsetY: 10,
           style: {
             fontSize: "17px",
-            fontWeight: "bold",
-          },
-        },
+            fontWeight: "bold"
+          }
+        }
       }}
     />
   );
@@ -103,10 +78,10 @@ const Fertilizer = () => {
   );
 
   if (loading) return <Preloader />;
-  if (error) return <img src={ErrorGif} alt={ErrorGif}/>;
+  if (error) return <img src={ErrorGif} alt={ErrorGif} />;
 
-  var EcY = (data.ec_history.map(({y})=>y));
-  var EcX = (data.ec_history.map(({ x }) => x));
+  var EcY = data.ec_history.map(({ y }) => y);
+  var EcX = data.ec_history.map(({ x }) => x);
   console.log(EcY);
   console.log(EcX);
 
@@ -114,28 +89,28 @@ const Fertilizer = () => {
     <div style={{ backgroundColor: "#cad3de" }}>
       <AppName />
       <div className="sm-ml-0 md:ml-8 xl:ml-8 2xl:ml-8 sm:mt-0 md:mt-16 xl:mt-16 2xl:mt-16 sm:p-1 md:p-1 p-1">
-        <div className="col-span-4 bg-gray-400 gap-3 rounded shadow-md flex xl:m-4 align-items p-4">
+        <div className="grid xl:grid-cols-4 gap-3 xl:m-4 align-items p-2">
           {data.fertilizer_valves.map((valves, i) => (
-            <div className="bg-gray-400 rounded shadow-md">
+            <div className=" bg-gray-400 rounded shadow-md">
               <FertilizerValves key={i} valves={valves} />
             </div>
           ))}
         </div>
-        <div className="col-span-4 bg-gray-400 gap-1 rounded shadow-md flex-grow m-4 flex align-items p-4">
+        <div className="grid xl:grid-cols-4 gap-3 xl:m-4 align-items p-2">
           {data.ec_values.map((ec, i) => (
             <div className="bg-gray-400 rounded shadow-md">
               <ECValves key={i} ec={ec} />
             </div>
           ))}
         </div>
-        <div className="bg-gray-400 rounded shadow-md m-4 p-2">
-          <ChartViewer/>
+        <div className="m-4">
+          <ChartViewer />
         </div>
-        <div className="grid grid-cols-2 bg-gray-400 rounded shadow-md m-4 p-2">
-          <div className="bg-gray-400 rounded shadow-md m-4">
+        <div className="xl:grid grid-cols-2 block gap-4 p-2">
+          <div className="bg-gray-400 rounded shadow-md mb-4">
             <FertilizerBarChart data={data.fertilizer_bargraph} />
           </div>
-          <div className="bg-gray-400 rounded shadow-md m-4">
+          <div className="bg-gray-400 rounded shadow-md mb-4">
             <FertilizerPieChart data={data.fertilizer_pie_chart} />
           </div>
         </div>
