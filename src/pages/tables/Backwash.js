762c390f13a1f41c54763cae60c8e5b956d4083a
Grant <Grant@LODICONCPT.LOCAL>
2021-08-16 15:29:13 +0200

    Backwash widget

--- src/pages/tables/Backwash.js
+++ src/pages/tables/Backwash.js
@@ -39,13 +39,13 @@ const BorderLinearProgress = withStyles((theme) => ({
 const WashBack = ({ backwash }) => {
   return (
     <div className="bg-gray-400 rounded shadow-md p-2 flex gap-2">
-      <div className="shadow-md rounded p-3 bg-gray-400">
-        <div className="font-bold text-xl">{backwash.name}</div>
-        <div className="font-bold text-2xl">{backwash.status}</div>
+      <div className="shadow-md rounded p-3 bg-gray-400 w-100">
+        <div className="font-bold text-2xl">{backwash.name}</div>
+        <div className="font-bold text-xl">{backwash.status}</div>
         <div className="font-bold text-md text-red-400">{backwash.alarm}</div>
       </div>
       <div className="bg-gray-400 rounded shadow-md p-2 items-center flex justify-center">
-        <img src={fertilizer} alt={fertilizer} width="130rem" height="20rem" />
+        <img src={fertilizer} alt={fertilizer} width="100rem" height="50rem" />
       </div>
     </div>
   );
