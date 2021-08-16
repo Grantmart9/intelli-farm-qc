7f8870ece4b62e034545176df6929e1cdebda769
Grant <Grant@LODICONCPT.LOCAL>
2021-08-16 15:01:11 +0200

    backwash error page

--- src/pages/tables/Backwash.js
+++ src/pages/tables/Backwash.js
@@ -65,6 +65,15 @@ export const Backwash = () => {
         </div>
       </div>
     );
+  if (data === null)
+    return (
+      <div style={{ backgroundColor: "#cad3de" }}>
+        <AppName />
+        <div className="sm-ml-0 md:ml-8 xl:ml-8 2xl:ml-8 sm:mt-0 md:mt-16 xl:mt-16 2xl:mt-16 sm:p-1 md:p-1 p-1">
+          <img src={ErrorGif} alt={ErrorGif} width="100%" />
+        </div>
+      </div>
+    );
 
   return (
     <div style={{ backgroundColor: "#cad3de" }}>
