7ec8beae2e3004af314dd2fece7c6bf98f255808
Grant Marthinus <grant.marthinus@lodicon.co.za>
2021-10-05 18:32:49 +0200

    Irrigation control

--- src/pages/IrrigationControl.js
+++ src/pages/IrrigationControl.js
@@ -31,7 +31,7 @@ const EquipmentStatus = ({ data }) => {
     image = fertilizer;
   }
   return (
-    <div className="-py-2 px-2 mb-3">
+    <div className="-py-2 px-2 mb-3 p-2">
       <div className="font-bold text-2xl mb-2">{data.name}</div>
       <div className="grid grid-cols-2 -mb-4">
         <div className="grid grid-rows-3">
